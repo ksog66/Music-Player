50b1d0062bf334cc4e75414feb3e85e8d925e70e
ksog66 <ksog66@gmail.com>
2021-10-07 16:58:25 +0530

    Multiple song playing from raw resource folder

--- app/src/main/java/com/notchdev/apdplayer/MainActivity.kt
+++ app/src/main/java/com/notchdev/apdplayer/MainActivity.kt
@@ -9,38 +9,59 @@ import com.notchdev.apdplayer.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private var mPlayer: MediaPlayer? = null
     private lateinit var binding: ActivityMainBinding
+    private lateinit var songList:List<Int>
+    private var currentSongPos = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mPlayer = MediaPlayer.create(this, R.raw.excuses)
+        songList = listOf(R.raw.excuses,R.raw.saada,R.raw.toxic)
+        mPlayer = MediaPlayer.create(this,songList[0])
+
         binding.apply {
             seekBar.max = mPlayer?.duration!!
+            durationTv.text = getTimeString(mPlayer?.duration!!)
 
-            btnPlay.setOnClickListener {
-                playSong()
-            }
-            btnPause.setOnClickListener {
+            playPauseBtn.setOnClickListener {
                 if (mPlayer?.isPlaying ?: return@setOnClickListener) {
                     mPlayer?.pause()
+                    playPauseBtn.setImageResource(R.drawable.ic_play)
+                } else {
+                    playPauseBtn.setImageResource(R.drawable.ic_pause)
+                    playSong()
                 }
             }
-            btnStop.setOnClickListener {
-                stopSong()
+
+            prevSongBtn.setOnClickListener {
+                if(currentSongPos == 0) {
+                    currentSongPos = songList.size - 1
+                } else {
+                    currentSongPos--
+                }
+                startSong(songList[currentSongPos])
             }
 
+            nextSongBtn.setOnClickListener {
+                if (currentSongPos == songList.size - 1) {
+                    currentSongPos = 0
+                } else {
+                    currentSongPos++
+                }
+                startSong(songList[currentSongPos])
+            }
             seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(p0: SeekBar?, currPos: Int, fromUser: Boolean) {
                     if (fromUser) {
                         mPlayer?.seekTo(currPos)
-                        tvCurTime.text = getTimeString(currPos)
+                        progressTv.text = getTimeString(currPos)
                     }
                 }
 
@@ -53,19 +74,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun startSong(res:Int) {
+        mPlayer?.stop()
+        mPlayer = MediaPlayer.create(this,res)
+        playSong()
+    }
+
     fun playSong() {
-        mPlayer = mPlayer ?: MediaPlayer.create(this, R.raw.excuses)
         mPlayer?.start()
         binding.apply {
             seekBar.max = mPlayer?.duration!!
-            tvCurTime.text = getTimeString(mPlayer?.duration!!)
+            progressTv.text = getTimeString(mPlayer?.duration!!)
         }
 
         lifecycleScope.launch(Dispatchers.Main) {
             while(mPlayer!=null) {
                 val pos = mPlayer?.currentPosition
                 binding.apply {
-                    tvCurTime.text = getTimeString(pos!!)
+                    progressTv.text = getTimeString(pos!!)
                     seekBar.progress = pos
                     delay(100)
                 }
@@ -79,7 +105,7 @@ class MainActivity : AppCompatActivity() {
         mPlayer = null
         binding.apply {
             seekBar.progress = 0
-            tvCurTime.text = "0:00"
+            progressTv.text = "0:00"
         }
     }
 
