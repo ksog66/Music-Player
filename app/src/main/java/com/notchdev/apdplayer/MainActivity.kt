64804109a41e6cd2fddcc03e156e56896639d333
ksog66 <ksog66@gmail.com>
2021-10-07 14:21:19 +0530

    song playing from raw resource folder

--- app/src/main/java/com/notchdev/apdplayer/MainActivity.kt
+++ app/src/main/java/com/notchdev/apdplayer/MainActivity.kt
@@ -1,11 +1,98 @@
 package com.notchdev.apdplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.notchdev.apdplayer.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var mPlayer: MediaPlayer? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mPlayer = MediaPlayer.create(this, R.raw.excuses)
+        binding.apply {
+            seekBar.max = mPlayer?.duration!!
+
+            btnPlay.setOnClickListener {
+                playSong()
+            }
+            btnPause.setOnClickListener {
+                if (mPlayer?.isPlaying ?: return@setOnClickListener) {
+                    mPlayer?.pause()
+                }
+            }
+            btnStop.setOnClickListener {
+                stopSong()
+            }
+
+            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, currPos: Int, fromUser: Boolean) {
+                    if (fromUser) {
+                        mPlayer?.seekTo(currPos)
+                        tvCurTime.text = getTimeString(currPos)
+                    }
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                }
+            })
+        }
+    }
+
+    fun playSong() {
+        mPlayer = mPlayer ?: MediaPlayer.create(this, R.raw.excuses)
+        mPlayer?.start()
+        binding.apply {
+            seekBar.max = mPlayer?.duration!!
+            tvCurTime.text = getTimeString(mPlayer?.duration!!)
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            while(mPlayer!=null) {
+                val pos = mPlayer?.currentPosition
+                binding.apply {
+                    tvCurTime.text = getTimeString(pos!!)
+                    seekBar.progress = pos
+                    delay(100)
+                }
+            }
+        }
+
+    }
+
+    fun stopSong() {
+        mPlayer?.stop()
+        mPlayer = null
+        binding.apply {
+            seekBar.progress = 0
+            tvCurTime.text = "0:00"
+        }
+    }
+
+    private fun getTimeString(millis: Int): String {
+
+        val minutes = millis / 1000 / 60
+        val seconds = millis / 1000 % 60
+
+        var timeString = "$minutes:"
+        if(seconds < 10 ) {
+            timeString += "0"
+        }
+        timeString += "$seconds"
+        return timeString
     }
 }
